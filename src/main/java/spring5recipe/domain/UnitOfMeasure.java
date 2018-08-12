ce9b448bcd9c52722e50bb5e77c9b650a84bd66d
kamildev7 <k.93kamil@gmail.com>
2018-08-12 15:23:49 +0200

    Added data file in resources.

--- src/main/java/spring5recipe/domain/UnitOfMeasure.java
+++ src/main/java/spring5recipe/domain/UnitOfMeasure.java
@@ -13,7 +13,7 @@ public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String unitOfMeasure;
+    private String description;
 
     public Long getId() {
         return id;
@@ -23,11 +23,11 @@ public class UnitOfMeasure {
         this.id = id;
     }
 
-    public String getUnitOfMeasure() {
-        return unitOfMeasure;
+    public String getDescription() {
+        return description;
     }
 
-    public void setUnitOfMeasure(String unitOfMeasure) {
-        this.unitOfMeasure = unitOfMeasure;
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
